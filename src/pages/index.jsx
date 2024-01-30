3e94005ef2158c2fa52368c5bcf2d00f95a6b068
Enoch <chukwuemekambaebie@gmail.com>
2024-01-30 03:06:25 +0100

    update on date

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,7 +30,7 @@ export default function Home() {
       <div className="mt-[100px] relative">
         <img src="./images/join.png" className="rounded-md mt-4" alt="" />
         <div className="flex justify-between absolute lg:top-40 top-10 my-auto lg:left-20 left-5 lg:right-20 right-5">
-          <p className="text-white font-bold my-auto text-sm lg:text-5xl">Join us from May 2nd - 11th, 2024, at <br /> The Raddison Blue Hotel, Lagos.</p>
+          <p className="text-white font-bold my-auto text-sm lg:text-5xl">Join us from May 2<sup>nd</sup> - 11<sup>th</sup>, 2024, at <br /> The Raddison Blue Hotel,Ikeja - Lagos.</p>
           <img className="lg:w-32 w-14" src="./images/circular-button.png" alt="" />
         </div>
       </div>
