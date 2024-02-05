805cf65429f1eae87141cd83322bfa920fafb81b
Enoch Mbaebie <chukwuemekambaebie@gmail.com>
2024-02-05 15:22:22 +0100

    Update hackathon.jsx

--- src/pages/hackathon.jsx
+++ src/pages/hackathon.jsx
@@ -44,7 +44,7 @@ const hackathon = () => {
             <p className='py-20 text-2xl'>Lagos Blockchain week <br />
               (LBW) Hackathon</p>
             <button className="flex p-4 sm:w-full sm:mt-4 justify-center text-[#131313] rounded-md border bg-white px-8">
-             <p className="mr-2 my-auto"> Register <a href="https://forms.gle/Hz5RV1NCxZNfGbWQ6">for LBW Hackathon</a> </p>
+             <p className="mr-2 my-auto"> Register <a href="https://forms.gle/Hz5RV1NCxZNfGbWQ6"></a> </p>
               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
               </svg>
